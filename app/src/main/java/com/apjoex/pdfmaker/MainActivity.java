75212df16fa096591f192a382848833587f2065c
Akinde-peters Joseph <apjoex@gmail.com>
2017-06-25 20:46:56 +0100

    View inflation fixed

--- app/src/main/java/com/apjoex/pdfmaker/MainActivity.java
+++ app/src/main/java/com/apjoex/pdfmaker/MainActivity.java
@@ -5,25 +5,16 @@ import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.print.PrintAttributes;
-import android.print.pdf.PrintedPdfDocument;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -42,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         proceed_btn = (Button)findViewById(R.id.proceed_btn);
 
+        clickEvents();
+    }
+
+    private void clickEvents() {
         //
         proceed_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,41 +58,23 @@ public class MainActivity extends AppCompatActivity {
         // create a new document
         PdfDocument document = new PdfDocument();
 
-        // create a page description using A4 size dimensions
+        // create a page description
         PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder((int)Utils.convertPixelsToDp(2480,context), (int) Utils.convertPixelsToDp(3508,context), 1).create();
 
         // start a page
         PdfDocument.Page page = document.startPage(pageInfo);
 
-        Rect rect = new Rect();
-        rect.set(0, 0, (int)Utils.convertPixelsToDp(2480,context), (int)Utils.convertPixelsToDp(70,context));
+        int width, height;
+        width = (int)Utils.convertPixelsToDp(2480,context);
+        height = (int)Utils.convertPixelsToDp(3508,context);
 
-        //Make a new view and lay it out at the desired Rect dimensions
-        TextView view = new TextView(this);
-        view.setText("This is a custom drawn textview");
-        view.setBackgroundColor(Color.RED);
-        view.setGravity(Gravity.CENTER);
-
-        //Measure the view at the exact dimensions (otherwise the text won't center correctly)
-        int widthSpec = View.MeasureSpec.makeMeasureSpec(rect.width(), View.MeasureSpec.EXACTLY);
-        int heightSpec = View.MeasureSpec.makeMeasureSpec(rect.height(), View.MeasureSpec.EXACTLY);
+        View view = getLayoutInflater().inflate(R.layout.invoice,null);
+        int widthSpec = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY);
+        int heightSpec = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY);
         view.measure(widthSpec, heightSpec);
+        view.layout(0,0,width,height);
 
-        //Lay the view out at the rect width and height
-        view.layout(0, 0, rect.width(), rect.height());
-
-        Canvas canvas = page.getCanvas();
-
-        //Translate the Canvas into position and draw it
-        canvas.save();
-        canvas.translate(rect.left, rect.top);
-        view.draw(canvas);
-        canvas.translate(10,100);
-        canvas.drawText("This is a second line", 10, 10, new Paint());
-        canvas.translate(0, 40);
-        canvas.drawText(getString(R.string.long_text),10,10, new Paint());
-        canvas.translate(0,40);
-        getWindow().getDecorView().draw(canvas);
+        view.draw(page.getCanvas());
 
 
         document.finishPage(page);
